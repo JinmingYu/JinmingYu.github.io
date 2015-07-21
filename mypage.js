2c8ef890dc4f4924356a53872afa92a9818745c6
JinmingYu <jyu332@gatech.edu>
2015-07-20 20:25:28 -0400

    Actually content of my website

---
+++ mypage.js
@@ -0,0 +1,27 @@
+<script>
+$(document).ready(function() {
+    $('#tp1').click(function() {
+        $(this).css("div.content-color", "red")
+    })
+})
+$(document).ready(function() {
+    $('#tp2').click(function() {
+        $(this).css("div.content-color", "red")
+    })
+})
+$(document).ready(function() {
+    $('#tp3').click(function() {
+        $(this).css("div.content-color", "red")
+    })
+})
+$(document).ready(function() {
+    $('#tp4').click(function() {
+        $(this).css("div.content-color", "red")
+    })
+})
+$(document).ready(function() {
+    $('#tp5').click(function() {
+        $(this).css("div.content-color", "red")
+    })
+})
+</script>
